d7c353a20ca39986b43caa7c2da7c2070018f352
kyuhmi <kyunghomin@cpp.edu>
2025-03-22 00:24:42 -0700

    added Koin as a dependency for di

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,6 +58,8 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+
+            implementation(libs.koin.android)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -68,6 +70,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(libs.koin.core)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
