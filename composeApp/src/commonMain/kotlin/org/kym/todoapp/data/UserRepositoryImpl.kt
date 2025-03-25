f4ef1165aa45b57fc1f773edf627514d038fc8a5
kyuhmi <kyunghomin@cpp.edu>
2025-03-24 21:01:45 -0700

    hardcoded some test users

--- composeApp/src/commonMain/kotlin/org/kym/todoapp/data/UserRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/kym/todoapp/data/UserRepositoryImpl.kt
@@ -1,8 +1,18 @@
 package org.kym.todoapp.data
 
-class UserRepositoryImpl : UserRepository {
+class UserRepositoryImpl() : UserRepository {
 
     private val _users = arrayListOf<User>()
+
+    // todo: remove this init block when we have a real database
+    init {
+        _users.addAll(
+            listOf(
+                User("testUser"),
+                User("user123"),
+                User("user456"))
+        )
+    }
 
     override fun findUser(name: String): User? {
         return _users.firstOrNull { it.name == name }
